702b9cd84dc6f64afe92f4b838b18af3dcb86d9f
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-18 18:42:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
     },
     :names =>{
       :reading_railroad => {},
-      :pennsylvania_railraod => {},
+      :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline_railroad => {}
     }
