4bf6d76338f297158bf56680f6f038524659946c
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-18 18:30:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly_with_second_tier
   {
-  :railroads => [:pieces => "4"][:rent_in_dollars => {}][:name =>{}]
+  :railroads => {:pieces => 4,:rent_in_dollars => {},:name =>{}
 }
 
 end
